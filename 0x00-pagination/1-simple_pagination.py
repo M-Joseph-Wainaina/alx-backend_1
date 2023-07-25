146fb9600852f579f80db59f42425df04a444dea
wainaina <ynainajoseph@gmail.com>
2023-07-25 22:03:30 +0300

    pycodestyle errors removed

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -26,7 +26,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """function that return a paginated data"""   
+        """function that return a paginated data"""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
@@ -34,7 +34,6 @@ class Server:
         self.dataset()
         return self.__dataset[dataIndex[0]:dataIndex[1]]
 
-    
     def index_range(self, page, page_size):
         """function to do the above"""
         return ((page - 1) * page_size, page * page_size)
